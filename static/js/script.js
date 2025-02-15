ec539a83b7e1184c61c0eef4eea6be79e1c5bd24
convention-account <andreyslavutskiy@gmail.com>
2025-02-15 17:44:09 +0200

    new files

--- static/js/script.js
+++ static/js/script.js
@@ -14,3 +14,23 @@ function toggleMenu() {
     const navbar = document.querySelector('nav');
     navbar.classList.toggle('active');
 }
+
+// Function to show the alert
+function showAlert(message) {
+    const alert = document.getElementById("simple-alert");
+    const alertMessage = alert.querySelector(".alert-message");
+    alertMessage.textContent = message;
+    alert.style.display = "block";
+}
+
+// Function to close the alert
+function closeAlert() {
+    const alert = document.getElementById("simple-alert");
+    alert.style.display = "none";
+}
+
+// Example usage: Show alert when the page loads
+window.onload = () => {
+    showAlert("Увага! Фото ще завантажуються на сайт, вибачте за незручності!");
+};
+
